28ef2659669c74e8172f165fa47e9b18e512aeeb
MChavez2901 <chavezm17060@student.cghsnc.org>
2015-11-11 17:39:42 -0500

    Now up to date with eclipse project
    added button detection, color detection needs work on new ZTE cameras,
    will investigate changing the thresholds, working on comments

--- CameraOpenCVSkeleton/app/src/main/java/com/example/mtoebes/cameraopencv/MatFilter.java
+++ CameraOpenCVSkeleton/app/src/main/java/com/example/mtoebes/cameraopencv/MatFilter.java
@@ -240,7 +240,7 @@ public class MatFilter {
     public static Mat getHoughMat(Mat srcMat) {
         return getHoughMat(srcMat, hough_threshold, hough_minLinLength, hough_maxLineGap);
     }
-    public static Mat customColors(Mat srcMat)
+    public static  Mat customColors(Mat srcMat)
     {
         Bitmap bitmap = Bitmap.createBitmap(srcMat.cols(), srcMat.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(srcMat, bitmap);
@@ -292,8 +292,187 @@ public class MatFilter {
            }
        }
        Mat resMat1 = new Mat();
+        FindxStart(bitmap);
         Utils.bitmapToMat(bitmap, resMat1);
         return resMat1;
     }
+    private static void FindxStart( Bitmap bitmap ) {
+        int[] currentColor = new int[3];
+        int[] lastColor = new int[3];
+        int highestRed = 0;
+        int highestBlue = 0;
+        int highestRedx = 0;
+        int highestBluex = 0;
+        int bstreak = 0;
+        int rstreak = 0;
+        int lastBlue = 0;
+        int lastRed = 0;
+        int blueLine = 0;
+        int redLine = 0;
+        int firstblueTemp = 0;
+        int firstblue = 0;
+        int firstredTemp = 0;
+        int firstred = 0;
+        int lastbluetemp = 0;
+        int lastblue = 0;
+        int lastred = 0;
+        int lastredtemp = 0;
+        boolean firstBluePicked = false;
+        boolean firstRedPicked = false;
+        for (int x = 0; x < bitmap.getWidth(); x++) {
+            for (int y = 0; y < bitmap.getHeight(); y++) {
+                int maskRGB = bitmap.getPixel(x, y);
+                int currentBlue = Color.blue(maskRGB);
+                int currentRed = Color.red(maskRGB);
+                if (currentBlue == 255) {
+                    if (bstreak > 20) {
+                        lastbluetemp = y;
 
+                    }
+                    if (!firstBluePicked) {
+                        firstblueTemp = y;
+                        firstBluePicked = true;
+                    }
+                    if (lastBlue == 255) {
+                        bstreak++;
+                    }
+                    blueLine++;
+                    blueLine += bstreak;
+                } else {
+                    bstreak = 0;
+                }
+                if (currentRed == 255) {
+                    if (rstreak > 20) {
+                        lastredtemp = y;
+
+                    }
+                    if (!firstRedPicked) {
+                        firstredTemp = y;
+                        firstRedPicked = true;
+                    }
+                    if (lastRed == 255) {
+                        rstreak++;
+                    }
+                    redLine++;
+                    redLine += rstreak;
+                } else {
+                    rstreak = 0;
+                }
+                lastBlue = currentBlue;
+                lastRed = currentRed;
+            }
+            if (blueLine > highestBlue) {
+                highestBluex = x;
+                highestBlue = blueLine;
+                firstblue = firstblueTemp;
+                lastblue = lastbluetemp;
+
+            }
+            if (redLine > highestRed) {
+                highestRedx = x;
+                highestRed = redLine;
+                firstred = firstredTemp;
+                lastred = lastredtemp;
+
+            }
+            firstBluePicked = false;
+            firstRedPicked = false;
+            firstblueTemp = 0;
+            lastbluetemp = 0;
+            firstredTemp = 0;
+            lastredtemp = 0;
+            blueLine = 0;
+            redLine = 0;
+        }
+
+        //printHLine(firstred, 0);
+        //printHLine(lastred, 0);
+        // printHLine(firstblue, 0);
+        // printHLine(lastblue, 0);
+        findEdge(highestBluex, firstblue, lastred, "BLUE", bitmap);
+        findEdge(highestRedx, firstred, lastred, "RED", bitmap);
+
+    }
+    private static void findEdge(int x, int y1, int y2, String color, Bitmap bitmap) {
+        int[] currentColor;
+        int colorVal = 0;
+        int startx = 0;
+        int endx = 0;
+        for (int x1 = x; x1 > 0; x1 += -1) {
+            for (int y = y1; y < y2; y++) {
+
+                int maskRGB = bitmap.getPixel(x1, y);
+                int colortotal = Color.blue(maskRGB) + Color.red(maskRGB);
+                if (colortotal > 0) {
+                    colorVal++;
+                }
+
+            }
+            if (colorVal < ((y2 - y1) / 2)) {
+                startx = x1;
+                //printVLine(startx, 0);
+                break;
+            }
+            colorVal = 0;
+        }
+        colorVal = 0;
+
+        for (int x1 = x; x1 < bitmap.getWidth(); x1 += 1) {
+            for (int y = y1; y < y2; y++) {
+
+                int maskRGB = bitmap.getPixel(x1, y);
+                int colortotal = Color.blue(maskRGB) + Color.red(maskRGB);
+                if (colortotal > 0) {
+                    colorVal++;
+                }
+
+            }
+            if (colorVal < ((y2 - y1) / 4)) {
+                endx = x1;
+                //printVLine(endx, 0);
+                break;
+            }
+            colorVal = 0;
+        }
+        findButton(startx, endx, y1, y2, color, bitmap);
+    }
+    private static void findButton(int x1, int x2, int y1, int y2, String Color1, Bitmap bitmap) {
+        int blackVal = 0;
+        int offset = Math.abs(x1 - x2) / 4;
+        int yOffset = 2 * Math.abs(y1 - y2) / 3;
+        int[] currentColor = new int[3];
+        int blackxVal = 0;
+        int blackyVal = 0;
+
+        for (int x = (x1 + offset); x < (x2 - offset); x++) {
+            for (int y = (y1 + yOffset); y < y2; y++) {
+                int maskRGB = bitmap.getPixel(x,y);
+                int colortotal = Color.blue(maskRGB) + Color.red(maskRGB);
+                if (colortotal == 0) {
+                    blackxVal += x;
+                    blackyVal += y;
+                    blackVal++;
+                }
+            }
+        }
+        if (Math.abs(blackyVal*blackxVal) > 0) {
+            int buttonx = blackxVal / blackVal;
+            int buttony = blackyVal / blackVal;
+
+            printVLine(buttonx, 1, bitmap);
+            printHLine(buttony, 1, bitmap);
+        }
+    }
+    private static void printVLine(int xVal, int color, Bitmap bitmap) {
+
+        for (int z = 0; z < bitmap.getHeight(); z++) {
+            bitmap.setPixel(xVal, z, Color.argb(255,0,255,0));
+        }
+    }
+    private static void printHLine(int yVal, int color, Bitmap bitmap) {
+
+            for (int z = 0; z < bitmap.getWidth(); z++) {
+                bitmap.setPixel(z, yVal, Color.argb(255,0,255,0));
+        }
+    }
 }
